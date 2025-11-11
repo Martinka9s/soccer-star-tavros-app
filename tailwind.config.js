4ec4c00dcea8eb39b83085890ccd42369392df51
Martinka9s <163937173+Martinka9s@users.noreply.github.com>
2025-11-11 21:56:57 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: '#6B2FB5',
-          dark: '#5A28A0',
-          light: '#8B4FD5',
-        },
         dark: {
-          DEFAULT: '#1A1A2E',
-          lighter: '#2D2D44',
-          darker: '#0F0F1E',
+          DEFAULT: 'var(--color-bg)',
+          lighter: 'var(--color-bg-lighter)',
+        },
+        primary: {
+          DEFAULT: 'var(--color-primary)',
+          dark: 'var(--color-primary-dark)',
         },
       },
     },
