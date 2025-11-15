65154426adbf9882f8245e93c4f118673bbc081b
Martinka9s <163937173+Martinka9s@users.noreply.github.com>
2025-11-15 13:34:30 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -169,6 +169,7 @@ function App() {
                 setShowTeamRegistrationModal(true);
               }
             }}
+            user={user}
           />
         )}
         {activeTab === 'calendar' && (
